d7b598eab7df04295fab1c93f88ce2efccac0b4e
Marisa Mark <themarisamark@gmail.com>
2019-11-06 15:29:51 -0800

    adding api routes and Schema details

--- models/workout.js
+++ models/workout.js
@@ -3,8 +3,50 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now()
+    },
+
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+
+        weight: {
+          type: Number,
+        },
+
+        reps: {
+          type: Number,
+        },
+
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        }
+      }
+    ],
   },
   {
+
     toJSON: {
       // include any virtual properties when data is requested
       virtuals: true
@@ -13,7 +55,7 @@ const workoutSchema = new Schema(
 );
 
 // adds a dynamically-created property to schema
-workoutSchema.virtual("totalDuration").get(function() {
+workoutSchema.virtual("totalDuration").get(function () {
   // "reduce" array of exercises down to just the sum of their durations
   return this.exercises.reduce((total, exercise) => {
     return total + exercise.duration;
