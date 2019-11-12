1db1ad4684ed064fb8fc1a9537f4cde6bfae53ba
Marisa Mark <themarisamark@gmail.com>
2019-11-12 09:59:10 -0800

    adding heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -17,6 +17,13 @@ mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds061651.mlab.com:61651/heroku_g29hk984");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // routes
 app.use(require("./routes/api.js"));
